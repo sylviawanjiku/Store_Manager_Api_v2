ca6dd588c28385ea5cd61ed57e7d6267b87b3544
sylviawanjiku <sylviakatherine22@gmail.com>
2018-10-25 19:32:14 +0300

    [feature #161432039]connection to database

---
+++ app/api/v2/models_v2/db.py
@@ -0,0 +1,7 @@
+import psycopg2
+
+
+try:
+    conn = psycopg2.connect("dbname=Storemanager user=postgres password=root host=localhost")
+except Exception as e:
+        print("connection to database failed",e)

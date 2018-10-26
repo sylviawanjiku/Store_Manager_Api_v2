0c6acb12576f0282d6c1a871141f89fe3a96798a
sylviawanjiku <sylviakatherine22@gmail.com>
2018-10-26 14:06:34 +0300

    Database connection

--- app/api/v2/models_v2/db.py
+++ app/api/v2/models_v2/db.py
@@ -1,7 +1,17 @@
-# import psycopg2
+import psycopg2
+id = 4
+username = "username"
+email ="email"
+password = "password"
+conn = psycopg2.connect("dbname=Storemanager user=postgres password=root host=localhost")
 
+try:
+  
+    cursor = conn.cursor()
 
-# try:
-#     conn = psycopg2.connect("dbname=Storemanager user=postgres password=root host=localhost")
-# except Exception as e:
-#         print("connection to database failed",e)
+    cursor.execute())
+    rows = cursor.fetchone()
+    print(rows)
+   
+except Exception as e:
+        print("connection to database failed",e)

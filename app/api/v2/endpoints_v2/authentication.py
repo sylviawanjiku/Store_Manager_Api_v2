636ced795f0de9dc9513946b52fa3c897c0bc6d5
sylviawanjiku <sylviakatherine22@gmail.com>
2018-10-31 15:58:36 +0300

    [feature #161431079] sign out user

--- app/api/v2/endpoints_v2/authentication.py
+++ app/api/v2/endpoints_v2/authentication.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask_restful import Resource ,reqparse
 from werkzeug.security import check_password_hash
-from flask_jwt_extended import create_access_token,jwt_required ,get_jwt_identity
+from flask_jwt_extended import create_access_token,jwt_required ,get_jwt_identity,get_raw_jwt
 from ..models_v2.models import User
 from utils import validator
 import datetime
 from functools import wraps
 
-
+blacklist = set()
 # request data validation
 parser =reqparse.RequestParser()
 
@@ -56,6 +56,5 @@ class Logout(Resource):
     @jwt_required
     def delete(self):
             jti = get_raw_jwt()['jti']
-
             blacklist.add(jti)
             return {"msg": "Successfully logged out"}, 200
